a3a0effbaf40ce0ee2c51667346758d331acf18c
mudassar6969 <mudassar@tintash.com>
2021-06-14 00:16:22 +0500

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,5 @@
+module test1
+
+go 1.16
+
+require golang.org/x/net v0.0.0-20210610132358-84b48f89b13b // indirect
---
+++ test1.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+	"unicode"
+
+	"golang.org/x/net/html"
+)
+
+func findWordsCount(str string) int {
+
+	if len(str) == 0 {
+		return 0
+	}
+	count := 1
+	for _, r := range str {
+
+		if unicode.IsUpper(r) {
+			count++
+		}
+	}
+
+	return count
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func filesExercise(fileName, textToWrite string) {
+
+	file, fileCreateError := os.Create(fileName)
+	checkError(fileCreateError)
+
+	defer file.Close()
+
+	_, writeError := file.WriteString(textToWrite)
+	checkError(writeError)
+
+	readFile(fileName)
+}
+
+func readFile(fileName string) {
+	data, readError := ioutil.ReadFile(fileName)
+	checkError(readError)
+	fmt.Print(string(data))
+}
+
+type TokenData struct {
+	Href string
+	Text string
+}
+
+func parseFile(fileName string) []TokenData {
+
+	data, readError := ioutil.ReadFile(fileName)
+	checkError(readError)
+
+	var tokensData []TokenData
+
+	reader := strings.NewReader(string(data))
+
+	tokenizer := html.NewTokenizer(reader)
+
+	for {
+		tokenType := tokenizer.Next()
+
+		if tokenType == html.ErrorToken {
+			break
+		}
+
+		if tokenType == html.StartTagToken {
+			tagB, _ := tokenizer.TagName()
+			if string(tagB) == "a" {
+				_, valB, _ := tokenizer.TagAttr()
+				tokenizer.Next()
+
+				tokenData := TokenData{Href: string(valB), Text: tokenizer.Token().Data}
+				tokensData = append(tokensData, tokenData)
+			}
+		}
+	}
+
+	return tokensData
+
+}
+
+func main() {
+	// wordsCount := findWordsCount("saveChangesInTheEditor")
+	// fmt.Println("Words Count: ", wordsCount)
+
+	//filesExercise("sample.txt", "sample text to write")
+
+	fmt.Println(parseFile("ex4.html"))
+}

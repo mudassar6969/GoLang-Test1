b38f8c4349254a12c07ad54896954c5b977605ab
Mudassar Raza <mudassar@tintash.com>
2021-06-14 21:50:36 +0500

    Added more test cases for words count

--- training_test.go
+++ training_test.go
@@ -1,10 +1,33 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestFindWordsCount(t *testing.T) {
+func TestFindWordsCountBasic(t *testing.T) {
 	count := findWordsCount("saveChangesInTheEditor")
 	if count != 5 {
 		t.Error("count should be 5 instead of", count)
+	}
+}
+
+type WordsTest struct {
+	Word  string
+	Count int
+}
+
+func TestFindWordsCountTableDriven(t *testing.T) {
+	tests := []WordsTest{{"testDrivenDevelopment", 3}, {"testAgain", 2}, {"thisIsSimpleAgain", 4}}
+
+	for _, item := range tests {
+
+		testName := fmt.Sprintf("%s, %d", item.Word, item.Count)
+		t.Run(testName, func(t *testing.T) {
+			count := findWordsCount(item.Word)
+			if item.Count != count {
+				t.Error("Count is not correct", count)
+			}
+		})
 	}
 }
